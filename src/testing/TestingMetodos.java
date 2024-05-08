2ba5b8b317df14d2de88134a141f47934100928d
Andres <andresmatabuena15@gmail.com>
2024-05-08 19:51:43 +0200

    Commit final

--- src/testing/TestingMetodos.java
+++ src/testing/TestingMetodos.java
@@ -1,10 +1,25 @@
 package testing;
 
+import ana.Ana;
+import andres.Andres;
+import bryan.Bryan;
+import pedro.Pedro;
 
 public class TestingMetodos {
 
 	public static void main(String[] args) {
+		Ana ana = new Ana(); 
+		ana.LlamarPersona();
+		
+		Andres andres = new Andres();
+		andres.llamarPersona();
+		
+		Bryan bryan = new Bryan();
+		bryan.llamar_persona();
+		
+		Pedro pedro = new Pedro();
+		pedro.llamar_persona();
+		
 		
 	}
-
 }
